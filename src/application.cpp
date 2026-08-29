03a7c30750ad0a2fd3363971c054fd63d47ed6cb
Aubrey <valara.official@gmail.com>
2026-08-28 22:48:43 -0700

    initial sdl3 setup

--- src/application.cpp
+++ src/application.cpp
@@ -1,21 +1,49 @@
 #include "application.h"
-
-Application::Application() {
-
-}
-
-Application::~Application() {
-
-}
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 bool Application::initialize() {
-	return false;
+	if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+		SDL_Log("SDL initialization failed: %s", SDL_GetError());
+		return false;
+	}
+
+	window = SDL_CreateWindow("Vulkan Practice", width, height, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+
+	if (!window) {
+		SDL_Log("Window creation failed: %s", SDL_GetError());
+		return false;
+	}
+
+	return true;
 }
 
 void Application::run() {
+	running = true;
 
+	while (running) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			// Handle exit/quit
+			if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+				break;
+			}
+			
+			// Handle resize
+			if (event.type == SDL_EVENT_WINDOW_RESIZED) {
+				width = event.window.data1;
+				height = event.window.data2;
+				break;
+			}
+		}
+	}
 }
 
 void Application::shutdown() {
+	if (window) {
+		SDL_DestroyWindow(window);
+	}
 
+	SDL_QuitSubSystem(SDL_INIT_VIDEO);
 }
--- src/application.h
+++ src/application.h
@@ -1,11 +1,19 @@
 #pragma once
 
+struct SDL_Window;
+
 class Application {
 public:
-	Application();
-	~Application();
-
 	bool initialize();
 	void run();
 	void shutdown();
+
+private:
+	SDL_Window* window = nullptr;
+
+	static const int DEFAULT_WIDTH = 1280;
+	static const int DEFAULT_HEIGHT = 720;
+
+	uint32_t width = DEFAULT_WIDTH;
+	uint32_t height = DEFAULT_HEIGHT;
 };
